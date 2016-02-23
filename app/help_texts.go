9bcfb14376a5f2dbd3d1e43f784a5e8ad23f562f
moncho <ramon.cr@gmail.com>
2016-02-23 09:54:22 +0100

    Update help screen

--- app/help_texts.go
+++ app/help_texts.go
@@ -1,5 +1,11 @@
 package app
 
+import (
+	"fmt"
+
+	"github.com/moncho/dry/version"
+)
+
 //ShortHelp is a short description of dry
 const ShortHelp = `
 dry
@@ -9,15 +15,19 @@ then shows the list of containers and allows to execute Docker commands on them.
 
 `
 
-const help = `
-<white>dry</>
-
+var help = `
+<white>dry ` + fmt.Sprintf("version %s, build %s", version.VERSION, version.GITCOMMIT) + `</>` +
+	`
 Connects to a Docker daemon, shows the list of containers and allows to execute Docker commands on them.
 
 Visit <blue>http://moncho.github.io/dry/</> for more information.
 
-Container commands:
+<yellow>Basic commands</>
+<u>Command</u>     <u>Description                                </u>
+	<white>h</>         Shows this help screen
+	<white>Crtl+c</>    Quits dry inmediately
 
+<yellow>Container commands</>
 <u>Command</u>     <u>Description                                </u>
 	<white>F1</>        Cycles through containers sort modes (by Id | by Image | by Status | by Name)
 	<white>F2</>        Toggles showing all containers (default shows just running)
@@ -35,24 +45,24 @@ Container commands:
 	<white>q</>         Quits <white>dry</>.
 	<white>esc</>       In the main view, quits <white>dry</>. In any other view, goes back to the main view
 
-Image commands:
-
+<yellow>Image commands</>
 <u>Command</u>     <u>Description                                </u>
 	<white>F1</>        Cycles through images sort modes (by Repo | by Id | by Creation date | by Size)
 	<white>F5</>        Refresh the image list
 	<white>F10</>       Inspects Docker
 	<white>1</>         To container list
 	<white>Crtl+e</>    Removes the selected image
+	<white>Crtl+f</>    Forces removal of the selected image
 	<white>i</>         Shows image history
 	<white>Enter</>     Returns low-level information on the selected image
 	<white>q</>         Quits <white>dry</>.
 	<white>esc</>       In the main view, quits <white>dry</>. In any other view, goes back to the main view
 
-## Moving around in container/image list
+<yellow>Move around in container/image list</>
 	<white>ArrowUp</>   Moves the cursor one line up
 	<white>ArrowDown</> Moves the cursor uone line down
 
-## Moving around in logs/inspect
+<yellow>Move around in logs/inspect></>
 	<white>g</>         Moves the cursor to the beggining
 	<white>G</>         Moves the cursor until the end
 	<white>n</>         After a search, it moves forwards until the next search hit
@@ -63,9 +73,10 @@ Image commands:
 
 <r> Press ESC to exit help. </r>
 `
+
 const (
 	commonMappings = "<b>[H]:<darkgrey>Help</> <b>[Q]:<darkgrey>Quit</> <blue>|</> "
-	inspectMapping = "<b>[Enter]:<darkgrey>Inspect</>"
+	inspectMapping = "<b>[Enter]:<darkgrey>Inspect</></>"
 	keyMappings    = commonMappings +
 		"<b>[F1]:<darkgrey>Sort</> <b>[F2]:<darkgrey>Toggle Show Containers</> <b>[F5]:<darkgrey>Refresh</> <b>[F10]:<darkgrey>Docker Info</> <blue>|</> " +
 		"<b>[1]:<darkgrey>Images</> <b>[2]:<darkgrey>Networks</><blue>|</>" +
@@ -75,8 +86,8 @@ const (
 
 	imagesKeyMappings = commonMappings +
 		"<b>[F1]:<darkgrey>Sort</> <b>[F5]:<darkgrey>Refresh</> <b>[F10]:<darkgrey>Docker Info</> <blue>|</> " +
-		"<b>[1]:<darkgrey>Containers</> <b>[2]:<darkgrey>Networks</><blue> <blue>|</>" +
-		"<b>[Crtl+E]:<darkgrey>Remove</> <b>[I]:<darkgrey>History</> <blue>|</>" +
+		"<b>[1]:<darkgrey>Containers</> <b>[2]:<darkgrey>Networks</> <blue>|</>" +
+		"<b>[Crtl+E]:<darkgrey>Remove</> <b>[Crtl+F]:<darkgrey>Force Remove</> <b>[I]:<darkgrey>History</> <blue>|</>" +
 		inspectMapping
 
 	networkKeyMappings = commonMappings +
