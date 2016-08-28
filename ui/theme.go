b613807db07745bd42a4f3c53e689eaaf06f1d90
moncho <ramon.cr@gmail.com>
2016-08-28 14:26:51 +0200

    Move theme to ui package

---
+++ ui/theme.go
@@ -0,0 +1,18 @@
+package ui
+
+//ColorTheme represents a color theme
+type ColorTheme struct {
+	Fg           Color
+	Bg           Color
+	DarkBg       Color
+	Prompt       Color
+	Key          Color
+	Current      Color
+	CurrentMatch Color
+	Spinner      Color
+	Info         Color
+	Cursor       Color
+	Selected     Color
+	Header       Color
+	Footer       Color
+}
