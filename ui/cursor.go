199987dcf1d97210b64f00772c3b7d686604773b
moncho <ramon.cr@gmail.com>
2017-03-27 10:22:49 +0200

    Track direction of cursor (down/upwards)

---
+++ ui/cursor.go
@@ -0,0 +1,64 @@
+package ui
+
+import "sync"
+
+//Cursor represents the cursor position on the screen
+type Cursor struct {
+	pos       int
+	downwards bool
+	sync.RWMutex
+}
+
+//MovingDown returns true if the cursor is moving downwards or not
+//after the last movement.
+func (cursor *Cursor) MovingDown() bool {
+	return cursor.downwards
+}
+
+//Position tells on which screen pos the cursor is
+func (cursor *Cursor) Position() int {
+	cursor.RLock()
+	defer cursor.RUnlock()
+	return cursor.pos
+}
+
+//Reset sets the cursor in the initial position
+func (cursor *Cursor) Reset() {
+	cursor.Lock()
+	defer cursor.Unlock()
+	cursor.pos = 0
+	cursor.downwards = true
+
+}
+
+//ScrollCursorDown moves the cursor to the pos below the current one
+func (cursor *Cursor) ScrollCursorDown() {
+	cursor.Lock()
+	defer cursor.Unlock()
+	cursor.pos = cursor.pos + 1
+	cursor.downwards = true
+}
+
+//ScrollCursorUp moves the cursor to the pos above the current one
+func (cursor *Cursor) ScrollCursorUp() {
+	cursor.Lock()
+	defer cursor.Unlock()
+	if cursor.pos > 0 {
+		cursor.pos = cursor.pos - 1
+	} else {
+		cursor.pos = 0
+	}
+	cursor.downwards = false
+}
+
+//ScrollTo moves the cursor to the given pos
+func (cursor *Cursor) ScrollTo(pos int) {
+	cursor.Lock()
+	defer cursor.Unlock()
+	if pos > cursor.pos {
+		cursor.downwards = true
+	} else {
+		cursor.downwards = false
+	}
+	cursor.pos = pos
+}
