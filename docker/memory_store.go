693da39fcd7724b714f065ab171a2df8f8383a7d
moncho <ramon.cr@gmail.com>
2017-03-31 00:45:01 +0200

    Remove stutter

--- docker/memory_store.go
+++ docker/memory_store.go
@@ -15,8 +15,8 @@ type ContainerStore interface {
 	Size() int
 }
 
-// DockerContainerStore is an in-memory container store backed up by a Docker daemon.
-type DockerContainerStore struct {
+// InMemoryContainerStore is an in-memory container store backed up by a Docker daemon.
+type InMemoryContainerStore struct {
 	s      map[string]*types.Container
 	c      []*types.Container
 	client dockerAPI.ContainerAPIClient
@@ -25,12 +25,12 @@ type DockerContainerStore struct {
 
 //NewDockerContainerStore creates a new Docker container store that will use the given Docker
 //daemon client to retrieve container information.
-func NewDockerContainerStore(client dockerAPI.ContainerAPIClient) (*DockerContainerStore, error) {
+func NewDockerContainerStore(client dockerAPI.ContainerAPIClient) (ContainerStore, error) {
 	containers, err := containers(client)
 	if err != nil {
 		return nil, err
 	}
-	store := &DockerContainerStore{
+	store := &InMemoryContainerStore{
 		s:      make(map[string]*types.Container),
 		client: client,
 	}
@@ -40,7 +40,7 @@ func NewDockerContainerStore(client dockerAPI.ContainerAPIClient) (*DockerContai
 	return store, nil
 }
 
-func (c *DockerContainerStore) add(cont *types.Container) {
+func (c *InMemoryContainerStore) add(cont *types.Container) {
 	c.Lock()
 	//If a container with the given ID exists already it is replaced
 	if _, ok := c.s[cont.ID]; ok {
@@ -59,7 +59,7 @@ func (c *DockerContainerStore) add(cont *types.Container) {
 }
 
 // Get returns a container from the store by id.
-func (c *DockerContainerStore) Get(id string) *types.Container {
+func (c *InMemoryContainerStore) Get(id string) *types.Container {
 	c.RLock()
 	res := c.s[id]
 	c.RUnlock()
@@ -67,12 +67,12 @@ func (c *DockerContainerStore) Get(id string) *types.Container {
 }
 
 // List returns a list of containers from the store.
-func (c *DockerContainerStore) List() []*types.Container {
+func (c *InMemoryContainerStore) List() []*types.Container {
 	return c.all(nil)
 }
 
 // Remove removes a container from the store by id.
-func (c *DockerContainerStore) Remove(id string) {
+func (c *InMemoryContainerStore) Remove(id string) {
 	c.Lock()
 	delete(c.s, id)
 	for pos, container := range c.c {
@@ -85,7 +85,7 @@ func (c *DockerContainerStore) Remove(id string) {
 }
 
 // Sort sorts the store
-func (c *DockerContainerStore) Sort(mode SortMode) []*types.Container {
+func (c *InMemoryContainerStore) Sort(mode SortMode) []*types.Container {
 	c.RLock()
 	defer c.RUnlock()
 	containers := c.List()
@@ -94,18 +94,18 @@ func (c *DockerContainerStore) Sort(mode SortMode) []*types.Container {
 }
 
 // Size returns the number of containers in the store.
-func (c *DockerContainerStore) Size() int {
+func (c *InMemoryContainerStore) Size() int {
 	c.RLock()
 	defer c.RUnlock()
 	return len(c.c)
 }
 
 // Filter returns containers found in the store by the given filter.
-func (c *DockerContainerStore) Filter(filter ContainerFilter) []*types.Container {
+func (c *InMemoryContainerStore) Filter(filter ContainerFilter) []*types.Container {
 	return c.all(filter)
 }
 
-func (c *DockerContainerStore) all(filter ContainerFilter) []*types.Container {
+func (c *InMemoryContainerStore) all(filter ContainerFilter) []*types.Container {
 	c.RLock()
 	var containers []*types.Container
 	for _, cont := range c.c {
