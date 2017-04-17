eefd2fb46a447af27f9387e3dce201cd9a8aef4d
moncho <ramon.cr@gmail.com>
2017-04-17 02:09:47 +0200

    Add color theme for list elements

--- ui/theme.go
+++ ui/theme.go
@@ -2,17 +2,19 @@ package ui
 
 //ColorTheme represents a color theme
 type ColorTheme struct {
-	Fg           Color
-	Bg           Color
-	DarkBg       Color
-	Prompt       Color
-	Key          Color
-	Current      Color
-	CurrentMatch Color
-	Spinner      Color
-	Info         Color
-	Cursor       Color
-	Selected     Color
-	Header       Color
-	Footer       Color
+	Fg               Color
+	Bg               Color
+	DarkBg           Color
+	Prompt           Color
+	Key              Color
+	Current          Color
+	CurrentMatch     Color
+	Spinner          Color
+	Info             Color
+	Cursor           Color
+	Selected         Color
+	Header           Color
+	Footer           Color
+	ListItem         Color
+	SelectedListItem Color
 }
