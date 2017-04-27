a01a1fe018543d848fda8b0c5d31b93c837bb0e2
moncho <ramon.cr@gmail.com>
2017-04-27 09:30:57 +0200

    Minor

---
+++ docker/prune_report.go
@@ -0,0 +1,19 @@
+package docker
+
+import "github.com/docker/docker/api/types"
+
+//PruneReport represents the result of a prune operation
+type PruneReport struct {
+	ContainerReport types.ContainersPruneReport
+	ImagesReport    types.ImagesPruneReport
+	NetworksReport  types.NetworksPruneReport
+	VolumesReport   types.VolumesPruneReport
+}
+
+//TotalSpaceReclaimed reports the total space reclaimed
+func (p *PruneReport) TotalSpaceReclaimed() uint64 {
+	total := p.ContainerReport.SpaceReclaimed
+	total += p.ImagesReport.SpaceReclaimed
+	total += p.VolumesReport.SpaceReclaimed
+	return total
+}
