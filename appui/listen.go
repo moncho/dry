6fe65e139763923bc15772e4a9edf111fe5ca0ad
moncho <ramon.cr@gmail.com>
2017-09-20 00:29:21 +0200

    Refresh only if a certain time has passed since the previous event

--- appui/listen.go
+++ appui/listen.go
@@ -2,17 +2,26 @@ package appui
 
 import (
 	"context"
+	"time"
 
 	"github.com/docker/docker/api/types/events"
 	"github.com/moncho/dry/docker"
 	"github.com/moncho/dry/ui/termui"
 )
 
+var timeBetweenRefresh = 500 * time.Millisecond
+
 //RegisterWidget registers the given widget for updates from the given source
 func RegisterWidget(source docker.SourceType, w termui.Widget) {
+	last := time.Now()
 	docker.GlobalRegistry.Register(
 		source,
 		func(ctx context.Context, message events.Message) error {
-			return w.Unmount()
+			if time.Now().Sub(last) > timeBetweenRefresh {
+				last = time.Now()
+
+				return w.Unmount()
+			}
+			return nil
 		})
 }
