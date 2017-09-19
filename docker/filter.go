3faefdde98469ee55001394c94cf97910bf15399
moncho <ramon.cr@gmail.com>
2017-09-19 09:29:50 +0200

    Add apply method to filters

--- docker/filter.go
+++ docker/filter.go
@@ -11,12 +11,12 @@ type ContainerFilter func(*Container) bool
 var ContainerFilters ContainerFilter
 
 //Unfiltered does not filter containers
-func (c ContainerFilter) Unfiltered() ContainerFilter {
+func (cf ContainerFilter) Unfiltered() ContainerFilter {
 	return func(c *Container) bool { return true }
 }
 
 //ByName filters containers by name
-func (c ContainerFilter) ByName(name string) ContainerFilter {
+func (cf ContainerFilter) ByName(name string) ContainerFilter {
 	return func(c *Container) bool {
 		for _, containerName := range c.Names {
 
@@ -29,34 +29,34 @@ func (c ContainerFilter) ByName(name string) ContainerFilter {
 }
 
 //ByID filters containers by ID
-func (c ContainerFilter) ByID(id string) ContainerFilter {
+func (cf ContainerFilter) ByID(id string) ContainerFilter {
 	return func(c *Container) bool {
 		return strings.Contains(c.ID, id)
 	}
 }
 
 //ByRunningState filters containers by its running state
-func (c ContainerFilter) ByRunningState(running bool) ContainerFilter {
+func (cf ContainerFilter) ByRunningState(running bool) ContainerFilter {
 	return func(c *Container) bool {
 		return IsContainerRunning(c) == running
 	}
 }
 
 //Running filters out container that are not running
-func (c ContainerFilter) Running() ContainerFilter {
-	return c.ByRunningState(true)
+func (cf ContainerFilter) Running() ContainerFilter {
+	return cf.ByRunningState(true)
 }
 
 //NotRunning filters out container that are running
-func (c ContainerFilter) NotRunning() ContainerFilter {
-	return c.ByRunningState(false)
+func (cf ContainerFilter) NotRunning() ContainerFilter {
+	return cf.ByRunningState(false)
 }
 
-//Filter applies the given filter to the given slice of containers
-func Filter(c []*Container, filter ContainerFilter) []*Container {
+//Apply applies this filter to the given slice of containers
+func (cf ContainerFilter) Apply(c []*Container) []*Container {
 	var containers []*Container
 	for _, cont := range c {
-		if filter == nil || filter(cont) {
+		if cf(cont) {
 			containers = append(containers, cont)
 		}
 	}
