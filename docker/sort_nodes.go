b5ce4ea8e86c17c411fead756364d6b43a047111
moncho <ramon.cr@gmail.com>
2017-09-24 19:36:46 +0200

    Add sorting for nodes

---
+++ docker/sort_nodes.go
@@ -0,0 +1,69 @@
+package docker
+
+import (
+	"sort"
+
+	"github.com/docker/docker/api/types/swarm"
+)
+
+//Allowed sort methods
+const (
+	NoSortNode SortMode = iota
+	SortByNodeName
+	SortByNodeRole
+	SortByNodeCPU
+	SortByNodeMem
+	SortByNodeStatus
+)
+
+type swarmNodes []swarm.Node
+
+func (s swarmNodes) Len() int      { return len(s) }
+func (s swarmNodes) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+type nodesByName struct{ swarmNodes }
+
+func (s nodesByName) Less(i, j int) bool {
+	return s.swarmNodes[i].Description.Hostname < s.swarmNodes[j].Description.Hostname
+}
+
+type nodesByRole struct{ swarmNodes }
+
+func (s nodesByRole) Less(i, j int) bool {
+	return s.swarmNodes[i].Spec.Role < s.swarmNodes[j].Spec.Role
+}
+
+type nodesByCPU struct{ swarmNodes }
+
+func (s nodesByCPU) Less(i, j int) bool {
+	return s.swarmNodes[i].Description.Resources.NanoCPUs < s.swarmNodes[j].Description.Resources.NanoCPUs
+}
+
+type nodesByMem struct{ swarmNodes }
+
+func (s nodesByMem) Less(i, j int) bool {
+	return s.swarmNodes[i].Description.Resources.MemoryBytes < s.swarmNodes[j].Description.Resources.MemoryBytes
+}
+
+type nodesByStatus struct{ swarmNodes }
+
+func (s nodesByStatus) Less(i, j int) bool {
+	return s.swarmNodes[i].Status.State < s.swarmNodes[j].Status.State
+}
+
+//SortNodes sorts the given nodes slice using the given mode
+func SortNodes(nodes []swarm.Node, mode SortMode) {
+	switch mode {
+	case SortByNodeName:
+		sort.Sort(nodesByName{nodes})
+	case SortByNodeRole:
+		sort.Sort(nodesByRole{nodes})
+	case SortByNodeCPU:
+		sort.Sort(nodesByCPU{nodes})
+	case SortByNodeMem:
+		sort.Sort(nodesByMem{nodes})
+	case SortByNodeStatus:
+		sort.Sort(nodesByStatus{nodes})
+	}
+
+}
---
+++ docker/sort_nodes_test.go
@@ -0,0 +1,138 @@
+package docker
+
+import (
+	"testing"
+
+	"github.com/docker/docker/api/types/swarm"
+)
+
+func TestSortNodes(t *testing.T) {
+	type args struct {
+		nodes []swarm.Node
+		mode  SortMode
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"Sort by name ",
+			args{
+				[]swarm.Node{
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname: "b",
+						},
+					},
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname: "a",
+						},
+					},
+				},
+				SortByNodeName,
+			},
+		},
+		{
+			"SortByNodeRole",
+			args{
+				[]swarm.Node{
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname: "b",
+						},
+						Spec: swarm.NodeSpec{
+							Role: "b",
+						},
+					},
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname: "a",
+						},
+						Spec: swarm.NodeSpec{
+							Role: "a",
+						},
+					},
+				},
+				SortByNodeRole,
+			},
+		},
+		{
+			"SortByNodeCPU ",
+			args{
+				[]swarm.Node{
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname:  "b",
+							Resources: swarm.Resources{NanoCPUs: 2},
+						},
+					},
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname:  "a",
+							Resources: swarm.Resources{NanoCPUs: 1},
+						},
+					},
+				},
+				SortByNodeCPU,
+			},
+		},
+		{
+			"SortByNodeMem ",
+			args{
+				[]swarm.Node{
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname:  "b",
+							Resources: swarm.Resources{MemoryBytes: 2},
+						},
+					},
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname:  "a",
+							Resources: swarm.Resources{MemoryBytes: 1},
+						},
+					},
+				},
+				SortByNodeMem,
+			},
+		},
+		{
+			"SortByNodeStatus ",
+			args{
+				[]swarm.Node{
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname: "b",
+						},
+						Status: swarm.NodeStatus{
+							State: "b",
+						},
+					},
+					swarm.Node{
+						Description: swarm.NodeDescription{
+							Hostname: "a",
+						},
+						Status: swarm.NodeStatus{
+							State: "a",
+						},
+					},
+				},
+				SortByNodeStatus,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nodes := tt.args.nodes
+			SortNodes(nodes, tt.args.mode)
+			if nodes[0].Description.Hostname != "a" {
+				t.Errorf("Unexpected node as first element %v", nodes[0])
+			}
+			//Hostname is used to check ordering for any testcase
+			if nodes[0].Description.Hostname > nodes[1].Description.Hostname {
+				t.Errorf("Unexpected node ordering %v", nodes)
+			}
+		})
+	}
+}
