2493f7b96253801f7a201cc13ad75edfc7f85c9c
moncho <ramon.cr@gmail.com>
2017-09-28 09:56:28 +0200

    Create sort modes for services

---
+++ docker/sort_services.go
@@ -0,0 +1,44 @@
+package docker
+
+import (
+	"sort"
+
+	"github.com/docker/docker/api/types/swarm"
+)
+
+//Allowed sort methods
+const (
+	NoSortService SortMode = iota
+	SortByServiceName
+	SortByServiceImage
+)
+
+type swarmServices []swarm.Service
+
+func (s swarmServices) Len() int      { return len(s) }
+func (s swarmServices) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+type servicesByName struct{ swarmServices }
+
+func (s servicesByName) Less(i, j int) bool {
+
+	return s.swarmServices[i].Spec.Name < s.swarmServices[j].Spec.Name
+}
+
+type servicesByImage struct{ swarmServices }
+
+func (s servicesByImage) Less(i, j int) bool {
+	return s.swarmServices[i].Spec.TaskTemplate.ContainerSpec.Image < s.swarmServices[j].Spec.TaskTemplate.ContainerSpec.Image
+}
+
+//SortServices sorts the given service slice using the given mode
+func SortServices(services []swarm.Service, mode SortMode) {
+	switch mode {
+	case SortByServiceName:
+		sort.SliceStable(servicesByName{services}.swarmServices, servicesByName{services}.Less)
+	case SortByServiceImage:
+		sort.SliceStable(servicesByImage{services}.swarmServices, servicesByImage{services}.Less)
+
+	}
+
+}
---
+++ docker/sort_services_test.go
@@ -0,0 +1,104 @@
+package docker
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/docker/docker/api/types/swarm"
+)
+
+func TestSortServices(t *testing.T) {
+	type args struct {
+		services []swarm.Service
+		mode     SortMode
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"SortByServiceName",
+			args{
+				[]swarm.Service{
+					swarm.Service{
+						Spec: swarm.ServiceSpec{
+							Annotations: swarm.Annotations{Name: "a"},
+						},
+					},
+					swarm.Service{
+						Spec: swarm.ServiceSpec{
+							Annotations: swarm.Annotations{Name: "c"},
+						},
+					},
+					swarm.Service{
+						Spec: swarm.ServiceSpec{
+							Annotations: swarm.Annotations{Name: "b"},
+						},
+					},
+				},
+				SortByServiceName,
+			},
+		},
+		{
+			"SortByServiceImage",
+			args{
+				[]swarm.Service{
+					swarm.Service{
+						Spec: swarm.ServiceSpec{
+							Annotations: swarm.Annotations{Name: "b"},
+							TaskTemplate: swarm.TaskSpec{
+								ContainerSpec: swarm.ContainerSpec{
+									Image: "b",
+								},
+							},
+						},
+					},
+					swarm.Service{
+						Spec: swarm.ServiceSpec{
+							Annotations: swarm.Annotations{Name: "c"},
+							TaskTemplate: swarm.TaskSpec{
+								ContainerSpec: swarm.ContainerSpec{
+									Image: "c",
+								},
+							},
+						},
+					},
+					swarm.Service{
+						Spec: swarm.ServiceSpec{
+							Annotations: swarm.Annotations{Name: "a"},
+							TaskTemplate: swarm.TaskSpec{
+								ContainerSpec: swarm.ContainerSpec{
+									Image: "a",
+								},
+							},
+						},
+					},
+				},
+				SortByServiceImage,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			services := tt.args.services
+			SortServices(services, tt.args.mode)
+
+			if !sort.IsSorted(toSortInterface(services, tt.args.mode)) {
+				t.Errorf("Unexpected service ordering %v", services)
+			}
+		})
+	}
+}
+
+func toSortInterface(services []swarm.Service, mode SortMode) sort.Interface {
+
+	switch mode {
+	case SortByServiceName:
+		return servicesByName{services}
+	case SortByServiceImage:
+		return servicesByImage{services}
+	default:
+		return nil
+	}
+
+}
