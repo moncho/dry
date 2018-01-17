8ad914b732eb5583b61b95e6b3514cf9f89ed6d4
moncho <ramon.cr@gmail.com>
2018-01-17 23:10:38 +1300

    Remove unneded code

--- ui/focus.go
+++ ui/focus.go
@@ -4,5 +4,5 @@ import "github.com/nsf/termbox-go"
 
 //Focusable define ui elements that can focus
 type Focusable interface {
-	Focus(events chan termbox.Event, done chan<- struct{})
+	Focus(events <-chan termbox.Event, done func()) error
 }
