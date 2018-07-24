4f3abcf52afb368526d962550b46f6628ea75926
moncho <50428+moncho@users.noreply.github.com>
2018-07-24 02:26:53 +0200

    Add check for nil stats
    The row will not be updated in this case. Fixes #64.

--- appui/stats_row_test.go
+++ appui/stats_row_test.go
@@ -1,10 +1,14 @@
 package appui
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	units "github.com/docker/go-units"
 	"github.com/moncho/dry/docker"
+	drytermui "github.com/moncho/dry/ui/termui"
 )
 
 func TestStatsRow(t *testing.T) {
@@ -50,5 +54,114 @@ func TestStatsRow(t *testing.T) {
 	}
 	if row.Uptime.Text != "-" {
 		t.Errorf("Uptime widget does not contain the default value. Expected: %s, got: %s.", "-", row.Uptime.Text)
+	}
+}
+
+func TestContainerStatsRow_Update(t *testing.T) {
+	type fields struct {
+		Status *drytermui.ParColumn
+		Name   *drytermui.ParColumn
+		ID     *drytermui.ParColumn
+		CPU    *drytermui.GaugeColumn
+		Memory *drytermui.GaugeColumn
+		Net    *drytermui.ParColumn
+		Block  *drytermui.ParColumn
+		Pids   *drytermui.ParColumn
+		Uptime *drytermui.ParColumn
+	}
+	type args struct {
+		container *docker.Container
+		stats     *docker.Stats
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			"Update row, row has the expected values",
+			fields{
+				Status: drytermui.NewParColumn(""),
+				Name:   drytermui.NewParColumn(""),
+				ID:     drytermui.NewParColumn(""),
+				CPU:    &drytermui.GaugeColumn{},
+				Memory: &drytermui.GaugeColumn{},
+				Net:    drytermui.NewParColumn(""),
+				Block:  drytermui.NewParColumn(""),
+				Pids:   drytermui.NewParColumn(""),
+				Uptime: drytermui.NewParColumn(""),
+			},
+			args{
+				container: &docker.Container{
+					Container: types.Container{ID: "CID", Names: []string{"Name"}, Status: "Never worked"},
+					ContainerJSON: types.ContainerJSON{
+						ContainerJSONBase: &types.ContainerJSONBase{
+							State: &types.ContainerState{
+								Status: "Running I guess",
+							}},
+					}},
+				stats: &docker.Stats{
+					PidsCurrent:   3,
+					NetworkRx:     1.15,
+					NetworkTx:     2.34,
+					CPUPercentage: 45.356,
+				},
+			},
+		},
+		{
+			"Update row, no stats are passed, row does not change",
+			fields{
+				Status: drytermui.NewParColumn(""),
+				Name:   drytermui.NewParColumn(""),
+				ID:     drytermui.NewParColumn(""),
+				CPU:    &drytermui.GaugeColumn{},
+				Memory: &drytermui.GaugeColumn{},
+				Net:    drytermui.NewParColumn(""),
+				Block:  drytermui.NewParColumn(""),
+				Pids:   drytermui.NewParColumn(""),
+				Uptime: drytermui.NewParColumn(""),
+			},
+			args{
+				container: &docker.Container{
+					Container: types.Container{ID: "CID", Names: []string{"Name"}, Status: "Never worked"},
+					ContainerJSON: types.ContainerJSON{
+						ContainerJSONBase: &types.ContainerJSONBase{
+							State: &types.ContainerState{
+								Status: "Running I guess",
+							}},
+					}},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			row := &ContainerStatsRow{
+				Status: tt.fields.Status,
+				Name:   tt.fields.Name,
+				ID:     tt.fields.ID,
+				CPU:    tt.fields.CPU,
+				Memory: tt.fields.Memory,
+				Net:    tt.fields.Net,
+				Block:  tt.fields.Block,
+				Pids:   tt.fields.Pids,
+				Uptime: tt.fields.Uptime,
+			}
+			stats := tt.args.stats
+			row.Update(tt.args.container, stats)
+			if stats != nil {
+				if row.Pids.Text != strconv.Itoa(int(stats.PidsCurrent)) {
+					t.Errorf("Unexpected pids. Got %s, expected %d", row.Pids.Text, stats.PidsCurrent)
+				}
+				net := fmt.Sprintf("%s / %s", units.BytesSize(stats.NetworkRx), units.BytesSize(stats.NetworkTx))
+				if row.Net.Text != net {
+					t.Errorf("Unexpected network information. Got %s, expected %s", row.Net.Text, net)
+				}
+
+				cpu := fmt.Sprintf("%.2f%%", stats.CPUPercentage)
+				if row.CPU.Label != cpu {
+					t.Errorf("Unexpected CPU information. Got %s, expected %s", row.CPU.Label, cpu)
+				}
+			}
+		})
 	}
 }
